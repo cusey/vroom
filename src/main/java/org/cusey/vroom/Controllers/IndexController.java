4102482e962f154687109c3ae3f992a69ea81aa7
john cusey <john_cusey@hotmail.com>
2019-04-22 15:18:04 -0400

    sent the dictionary object down from the database to the page

--- src/main/java/org/cusey/vroom/Controllers/IndexController.java
+++ src/main/java/org/cusey/vroom/Controllers/IndexController.java
@@ -1,13 +1,15 @@
 package org.cusey.vroom.Controllers;
 
 import org.cusey.vroom.Models.Dictionary;
+import org.cusey.vroom.Services.DictionaryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -15,6 +17,8 @@ public class IndexController {
 
     private final static Logger slf4jLogger = LoggerFactory.getLogger(IndexController.class);
 
+    @Autowired
+    private DictionaryService dictionaryService;
 
     //http://localhost:8095/
     @RequestMapping("/")
@@ -22,7 +26,12 @@ public class IndexController {
 
         slf4jLogger.debug("showIndex");
 
+        List<Dictionary> dictionaryList = dictionaryService.findAll();
+        request.setAttribute("Dictionary", dictionaryList);
+
+
         return "index";
     }
+
 
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
 
+    var dic = [];
+
+    var table = document.getElementById('dictionary-entries');
+
+    var rowLength = table.rows.length;
+
+    for(var x=0; x<rowLength; x+=1){
+        var row = table.rows[x];
+        dic[x] = [];
+
+        for(var y = 0; y < 7; y++){
+            dic[x][y] = row.cells[y].innerText;
+        }
+
+    }
+
 });
